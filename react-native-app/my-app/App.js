96490efd6c17d6b2d5ac4d4267cc0b033ac3f995
angeluk00700 <angeluk00700@gmail.com>
2023-06-24 18:14:17 +0900

    리액트 네이티브 연습

--- react-native-app/my-app/App.js
+++ react-native-app/my-app/App.js
@@ -1,23 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
-import CustomButton from './CustomButton';
+import { StatusBar } from "expo-status-bar";
+import CustomButton from "./CustomButton";
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.square}>
-        <Text style={[styles.Text, {fontWeight:'bold'}]}>App</Text>
+        <Text style={[styles.Text, { fontWeight: "bold" }]}>App</Text>
       </View>
       <Text></Text>
-      <Text style={[styles.Text, {color:'black'}]}>welcome to my app</Text>
+      <Text style={[styles.Text, { color: "black" }]}>welcome to my app</Text>
       <Text></Text>
+
       <CustomButton
-        title='button'
-        titleColor='white'
-        butt
-        onPress={() => Alert.alert('버튼 클릭')}
-        // onPress={() => alert('회원가입 버튼')}
+        title="button1"
+        titleColor="white"
+        onPress={() => alert("버튼 클릭")}
+        buttonColor="green"
+        disabled="false"
       />
+      <CustomButton title="button2" titleColor="white" />
+      <Button
+        title="button3"
+        buttonColor="#f194ff"
+        fontSize="100"
+        // disabled
+        onPress={() => alert("disabled button")}
+      ></Button>
     </View>
   );
 }
@@ -25,15 +41,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   square: {
     width: 200,
     height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4CAF50',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#4CAF50",
   },
   Text: {
     color: "white",
@@ -43,5 +59,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "red",
     padding: 10,
-  }
+  },
 });
