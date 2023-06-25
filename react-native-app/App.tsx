292091fb0e1bd77bbe3ae2d1f06616981a2480d5
angeluk00700 <angeluk00700@gmail.com>
2023-06-25 16:35:18 +0900

    리액트 네이티브 - hook 연습
    버튼 활성화

--- react-native-app/App.tsx
+++ react-native-app/App.tsx
@@ -1,6 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import React, {useState} from 'react';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
 
 // export default function App() {
 //   return (
@@ -12,56 +19,71 @@ import React, {useState} from 'react';
 // }
 
 const App = () => {
-  const [isBtnDisable='true', setBtnDisable] = useState(true);
+  const [isBtnDisable, setBtnDisable] = useState<boolean>(false);
+  const clickButton = () => {
+    Alert.alert("clicked");
+    setBtnDisable(false);
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
       <TouchableOpacity
-        style={[styles.button, isBtnDisable && styles.activeButton]}
-        disabled={!isBtnDisable}
+        style={[
+          styles.button,
+          isBtnDisable ? styles.disabledButton : styles.activeButton,
+        ]}
+        // style={[styles.button]}
+        disabled={isBtnDisable}
         onPress={() => {
-          setBtnDisable(false)
+          setBtnDisable(true);
+          // clickBtnABC();
         }}
       >
-        <Text style={styles.buttonText}>buttonAAA</Text>
+        <Text style={styles.buttonText}>buttonABC</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={[styles.button, isBtnDisable && styles.activeButton]}
+        style={[
+          styles.button,
+          // buttonABC가 활성화인 경우에는 비활성화, 비활성화인 경우에는 활성화로 표시
+          isBtnDisable ? styles.activeButton : styles.disabledButton,
+        ]}
         disabled={!isBtnDisable}
         onPress={() => {
-          setBtnDisable(true)
+          setBtnDisable(false);
         }}
       >
         <Text style={styles.buttonText}>buttonA활성화</Text>
       </TouchableOpacity>
+
+      <Text>버튼 disabled 상황 1 : {isBtnDisable ? "true" : "false"}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    alignItems: 'center',
-    backgroundColor: '#DCEDC8',
+    alignItems: "center",
+    backgroundColor: "sky",
     padding: 10,
     margin: 10,
     borderRadius: 5,
   },
   disabledButton: {
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     opacity: 0.5,
   },
   activeButton: {
-    backgroundColor: '#B2EBF2',
+    backgroundColor: "green",
   },
   buttonText: {
-    color: '#000',
+    color: "#000",
   },
 });
 
