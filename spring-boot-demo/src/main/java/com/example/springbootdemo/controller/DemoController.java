047273fdf5b5a5f2def6ce958fb2eaaf3ac25a10
angeluk00700 <angeluk00700@gmail.com>
2023-07-09 21:11:23 +0900

    스프링부트 프로젝트 재작성

---
+++ spring-boot-demo/src/main/java/com/example/springbootdemo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+  @RequestMapping("/")
+  public String Home() {
+    return "hello world!";
+  }
+}
